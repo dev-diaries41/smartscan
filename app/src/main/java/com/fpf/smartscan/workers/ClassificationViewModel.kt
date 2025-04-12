ba6d944ae37ef5397cefa6fb23a55a1337fff32b
d41dev <d41dev@proton.me>
2025-04-12 07:50:44 +0100

    show organisation indicator when active

---
+++ app/src/main/java/com/fpf/smartscan/workers/ClassificationViewModel.kt
@@ -0,0 +1,38 @@
+package com.fpf.smartscan.workers
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+
+class ClassificationViewModel(
+     application: Application) : AndroidViewModel(application
+) {
+    private val workManager = WorkManager.getInstance(application)
+    private val workTag = "ClassificationBatchWorker"
+    private val _organisationActive = MutableLiveData(false)
+    val organisationActive: LiveData<Boolean> = _organisationActive
+
+    init {
+        observeWorkStatus()
+    }
+
+    private fun observeWorkStatus() {
+        workManager.getWorkInfosByTagLiveData(workTag).observeForever { infos ->
+            if (infos.isNullOrEmpty()) {
+                _organisationActive.postValue(false)
+                return@observeForever
+            }
+
+            val runningWorker = infos.firstOrNull { it.state == WorkInfo.State.RUNNING  || it.state == WorkInfo.State.ENQUEUED }
+            if (runningWorker != null) {
+                _organisationActive.postValue(true)
+
+            } else {
+                _organisationActive.postValue(false)
+            }
+        }
+    }
+}
