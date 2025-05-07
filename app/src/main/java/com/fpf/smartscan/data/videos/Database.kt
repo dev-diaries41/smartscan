a0cfc34fa69794e4bb4784e715f03132c3a904da
d41dev <d41dev@proton.me>
2025-05-07 13:02:16 +0100

    add video database

---
+++ app/src/main/java/com/fpf/smartscan/data/videos/Database.kt
@@ -0,0 +1,29 @@
+package com.fpf.smartscan.data.videos
+
+import android.app.Application
+import androidx.room.*
+
+@Database(entities = [VideoEmbedding::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class VideoEmbeddingDatabase : RoomDatabase() {
+    abstract fun videoEmbeddingDao(): VideoEmbeddingDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: VideoEmbeddingDatabase? = null
+
+        fun getDatabase(application: Application): VideoEmbeddingDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    application,
+                    VideoEmbeddingDatabase::class.java,
+                    "video_embedding_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/fpf/smartscan/data/videos/Repository.kt
@@ -0,0 +1,22 @@
+package com.fpf.smartscan.data.videos
+
+import androidx.lifecycle.LiveData
+
+class VideoEmbeddingRepository(private val dao: VideoEmbeddingDao) {
+    val allVideoEmbeddings: LiveData<List<VideoEmbedding>> = dao.getAllVideoEmbeddings()
+
+    suspend fun getAllEmbeddingsSync(): List<VideoEmbedding> {
+        return dao.getAllEmbeddingsSync()
+    }
+    suspend fun insert(videoEmbedding: VideoEmbedding) {
+        dao.insertVideoEmbedding(videoEmbedding)
+    }
+
+    suspend fun delete(videoEmbedding: VideoEmbedding) {
+        dao.deleteVideoEmbedding(videoEmbedding)
+    }
+
+    suspend fun isVideoIndexed(id: Long): Boolean {
+        return dao.isVideoIndexed(id)
+    }
+}
