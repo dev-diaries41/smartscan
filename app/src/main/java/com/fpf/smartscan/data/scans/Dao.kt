ebebac4d7303f64a72a74537100e2a11bff3afe7
d41dev <d41dev@proton.me>
2025-05-18 22:19:46 +0100

    fix undo last scan logic by using in progress placeholder scan data and updating it

--- app/src/main/java/com/fpf/smartscan/data/scans/Dao.kt
+++ app/src/main/java/com/fpf/smartscan/data/scans/Dao.kt
@@ -9,13 +9,13 @@ interface ScanDataDao {
     @Query("SELECT * FROM scan_data ORDER BY date DESC")
     fun getAllScanData(): LiveData<List<ScanData>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertScanData(scanData: ScanData)
+    @Insert
+    suspend fun insert(scanData: ScanData): Long
 
     @Query("DELETE FROM scan_data where id = :id")
-    suspend fun deleteScanData(id: Int)
+    suspend fun delete(id: Int)
 
-    @Query("SELECT COALESCE(MAX(id), 0) FROM scan_data")
-    suspend fun getHighestId(): Int
+    @Query("UPDATE scan_data SET result = :result, date = :date WHERE id = :scanId")
+    suspend fun update(scanId: Int, result: Int, date: Long)
 }
 
