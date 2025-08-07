f5538b9d0f20589f56b70efd53110207b8f5bda6
d41dev <d41dev@proton.me>
2025-08-07 18:21:58 +0100

    fix grid view responsiveness and layout

--- app/src/main/java/com/fpf/smartscan/ui/screens/search/SearchResults.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/search/SearchResults.kt
@@ -3,15 +3,11 @@ package com.fpf.smartscan.ui.screens.search
 import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.*
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.fpf.smartscan.ui.components.ImageDisplay
 import com.fpf.smartscan.ui.components.MediaExpandedView
@@ -24,16 +20,14 @@ fun SearchResults(
     maxResults: Int,
     toggleViewResult: (uri: Uri?) -> Unit,
     type: MediaType,
+    numGridColumns: Int = 3,
 ) {
-    if(!isVisible) return
+    if (!isVisible) return
 
     val mainResult = searchResults.first()
     val similarResults = if (searchResults.size > 1) searchResults.drop(1).take(maxResults) else emptyList()
-    Column(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+
+    Column() {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
@@ -45,60 +39,56 @@ fun SearchResults(
                 uri = mainResult,
                 modifier = Modifier
                     .fillMaxSize()
-                    .clickable(onClick = { toggleViewResult(mainResult) }),
+                    .clickable { toggleViewResult(mainResult) },
                 contentScale = ContentScale.Crop,
-                type = type,
+                type = type
             )
         }
 
-        Spacer(modifier = Modifier.height(32.dp))
 
         Text(
             text = "Similar Results (${similarResults.size})",
+            textAlign = TextAlign.Left,
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.align(Alignment.Start)
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
         )
-        Spacer(modifier = Modifier.height(8.dp))
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(600.dp)  //  fixed height required to bounds the grid's maximum height.
-        ) {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(3),
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(8.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                items(similarResults) { uri ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(1f),
-                        shape = MaterialTheme.shapes.small,
-                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-                    ) {
-                        ImageDisplay(
-                            uri = uri,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clickable(onClick = { toggleViewResult(uri) }),
-                            contentScale = ContentScale.Crop,
-                            type = type
-                        )
+        Column() {
+            similarResults.chunked(numGridColumns).forEachIndexed { index, uris ->
+                val scale = (uris.size / numGridColumns.toFloat())
+                Row(
+                    modifier = Modifier.fillMaxSize(scale)
+                ) {
+                    uris.forEachIndexed { batchIdx, uri ->
+                        Card(
+                            modifier =  Modifier
+                                .weight((1 / numGridColumns.toFloat()))
+                                .aspectRatio(1f)
+                                .padding(4.dp),
+                            shape = MaterialTheme.shapes.small,
+                            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                        ) {
+                            ImageDisplay(
+                                uri = uri,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clickable { toggleViewResult(uri) },
+                                contentScale = ContentScale.Crop,
+                                type = type
+                            )
+                        }
                     }
                 }
             }
         }
     }
 
+    // Expanded view overlay
     if (resultToView != null) {
         MediaExpandedView(
             uri = resultToView,
             type = type,
-            onClose = {toggleViewResult(null)}
+            onClose = { toggleViewResult(null) }
         )
     }
 }
