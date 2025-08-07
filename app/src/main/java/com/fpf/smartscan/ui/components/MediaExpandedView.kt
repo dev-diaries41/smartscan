acacedc0d6c1fa3e3c61bcb5a4217574896025e8
d41dev <d41dev@proton.me>
2025-08-07 23:14:15 +0100

    add pointer taps for MediaExpandedView

--- app/src/main/java/com/fpf/smartscan/ui/components/MediaExpandedView.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/MediaExpandedView.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -33,6 +34,12 @@ import com.fpf.smartscan.lib.openImageInGallery
 import com.fpf.smartscan.lib.openVideoInGallery
 import com.fpf.smartscan.ui.screens.search.MediaType
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.input.pointer.pointerInput
+
 @Composable
 fun MediaExpandedView(
     uri: Uri,
@@ -40,16 +47,7 @@ fun MediaExpandedView(
     onClose: () -> Unit,
     maxSize: Int = DEFAULT_IMAGE_DISPLAY_SIZE
 ){
-    val context = LocalContext.current
-
-    val mime = context.contentResolver.getType(uri)
-    val shareIntent: Intent = Intent().apply {
-        this.action = Intent.ACTION_SEND
-        this.putExtra(Intent.EXTRA_STREAM, uri)
-        this.type = mime
-    }
-
-    var hideActions by remember { mutableStateOf(false) }
+    var isActionsVisible by remember { mutableStateOf(true) }
 
     Dialog(onDismissRequest = { onClose() }) {
         Box(
@@ -60,7 +58,13 @@ fun MediaExpandedView(
             if (type == MediaType.IMAGE) {
                 ImageDisplay(
                     uri = uri,
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onTap = { isActionsVisible = !isActionsVisible }
+                            )
+                        },
                     contentScale = ContentScale.Fit,
                     type = type,
                     maxSize = maxSize
@@ -69,60 +73,105 @@ fun MediaExpandedView(
                 VideoDisplay(
                     uri = uri,
                     modifier = Modifier.fillMaxSize(),
-                    onTap = { hideActions = !hideActions }
+                    onTap = { isActionsVisible = !isActionsVisible }
+                )
+            }
+            ActionRow(uri=uri, type=type, onClose, isVisible = isActionsVisible)
+        }
+    }
+}
+
+
+@Composable
+fun ActionRow(
+    uri: Uri,
+    type: MediaType,
+    onClose: () -> Unit,
+    isVisible: Boolean
+){
+    val context = LocalContext.current
+    val mime = context.contentResolver.getType(uri)
+    val shareIntent: Intent = Intent().apply {
+        this.action = Intent.ACTION_SEND
+        this.putExtra(Intent.EXTRA_STREAM, uri)
+        this.type = mime
+    }
+
+    ActionRowWithFade(visible = isVisible) {
+        IconButton(
+            onClick = { onClose() },
+            modifier = Modifier
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Close Image",
+                tint = Color.White
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        )
+        {
+            IconButton(onClick = {
+                if (type == MediaType.IMAGE) {
+                    openImageInGallery(context, uri)
+                } else {
+                    openVideoInGallery(context, uri)
+                }
+            }) {
+                Icon(
+                    Icons.Filled.PhotoLibrary,
+                    contentDescription = "Open in Gallery",
+                    tint = MaterialTheme.colorScheme.onSurface
                 )
             }
 
-            if (!hideActions) {
-                Row(
-                    modifier = Modifier.fillMaxWidth().padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top
-                ){
-                    IconButton(
-                        onClick = { onClose() },
-                        modifier = Modifier
-                    ) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Close Image",
-                            tint = Color.White
-                        )
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.End,
+            // Prevent share if mime is undefined for some reason
+            mime?.let {
+                IconButton(onClick = {
+                    context.startActivity(Intent.createChooser(shareIntent, null))
+                }) {
+                    Icon(
+                        Icons.Filled.Share,
+                        contentDescription = "Share",
+                        tint = MaterialTheme.colorScheme.onSurface
                     )
-                    {
-                        IconButton(onClick = {
-                            if (type == MediaType.IMAGE) {
-                                openImageInGallery(context, uri)
-                            } else {
-                                openVideoInGallery(context, uri)
-                            }
-                        }) {
-                            Icon(
-                                Icons.Filled.PhotoLibrary,
-                                contentDescription = "Open in Gallery",
-                                tint = MaterialTheme.colorScheme.onSurface
-                            )
-                        }
-
-                        // Prevent share if mime is undefined for some reason
-                        mime?.let {
-                            IconButton(onClick = {
-                                context.startActivity(Intent.createChooser(shareIntent, null))
-                            }) {
-                                Icon(
-                                    Icons.Filled.Share,
-                                    contentDescription = "Share",
-                                    tint = MaterialTheme.colorScheme.onSurface
-                                )
-                            }
-                        }
-                    }
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun ActionRowWithFade(
+    visible: Boolean,
+    content: @Composable () -> Unit
+) {
+    val alpha by animateFloatAsState(
+        targetValue = if (visible) 1f else 0f,
+        animationSpec = tween(durationMillis = 300)
+    )
+    val translationY by animateFloatAsState(
+        targetValue = if (visible) 0f else -30f,
+        animationSpec = tween(durationMillis = 300)
+    )
+
+    if (alpha > 0f) { // Optional optimization to skip drawing when fully invisible
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .graphicsLayer() {
+                    this.alpha = alpha
+                    this.translationY = translationY
+                }
+                .background(Color.Black.copy(alpha = 0.5f))
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            content()
         }
     }
 }
