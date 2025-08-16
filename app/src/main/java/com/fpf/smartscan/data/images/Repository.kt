46b15e894baf21ff65440bf4b7af4d1cd2677615
d41dev <d41dev@proton.me>
2025-08-16 15:06:39 +0100

    add file sync functions to repositories

--- app/src/main/java/com/fpf/smartscan/data/images/Repository.kt
+++ app/src/main/java/com/fpf/smartscan/data/images/Repository.kt
@@ -1,6 +1,12 @@
 package com.fpf.smartscan.data.images
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import com.fpf.smartscan.lib.clip.Embedding
+import com.fpf.smartscan.lib.clip.Embeddings
+import com.fpf.smartscan.lib.clip.saveEmbeddingsToFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ImageEmbeddingRepository(private val dao: ImageEmbeddingDao) {
     val allImageEmbeddingsEntity: LiveData<List<ImageEmbeddingEntity>> = dao.getAllImageEmbeddings()
@@ -26,4 +32,16 @@ class ImageEmbeddingRepository(private val dao: ImageEmbeddingDao) {
     suspend fun deleteAllEmbeddings() {
         dao.deleteAll()
     }
+
+    suspend fun getAllEmbeddingsWithFileSync(context: Context, filename: String): List<Embedding> {
+        val embeddings = dao.getAllEmbeddingsSync()
+        val mappedEmbeddings = embeddings.map { it.toEmbedding() }
+        if (mappedEmbeddings.isNotEmpty()) {
+            withContext(Dispatchers.IO) {
+                saveEmbeddingsToFile(context, filename, mappedEmbeddings)
+            }
+        }
+        return mappedEmbeddings
+    }
+
 }
--- app/src/main/java/com/fpf/smartscan/data/videos/Repository.kt
+++ app/src/main/java/com/fpf/smartscan/data/videos/Repository.kt
@@ -1,6 +1,12 @@
 package com.fpf.smartscan.data.videos
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import com.fpf.smartscan.data.images.toEmbedding
+import com.fpf.smartscan.lib.clip.Embedding
+import com.fpf.smartscan.lib.clip.saveEmbeddingsToFile
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class VideoEmbeddingRepository(private val dao: VideoEmbeddingDao) {
     val allVideoEmbeddingsEntity: LiveData<List<VideoEmbeddingEntity>> = dao.getAllVideoEmbeddings()
@@ -25,5 +31,15 @@ class VideoEmbeddingRepository(private val dao: VideoEmbeddingDao) {
 
     suspend fun deleteAllEmbeddings() {
         dao.deleteAll()
+    }
+    suspend fun getAllEmbeddingsWithFileSync(context: Context, filename: String): List<Embedding> {
+        val embeddings = dao.getAllEmbeddingsSync()
+        val mappedEmbeddings = embeddings.map { it.toEmbedding() }
+        if (mappedEmbeddings.isNotEmpty()) {
+            withContext(Dispatchers.IO) {
+                saveEmbeddingsToFile(context, filename, mappedEmbeddings)
+            }
+        }
+        return mappedEmbeddings
     }
 }
