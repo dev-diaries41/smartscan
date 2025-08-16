ffb251bfc2b12504237696018f107412c6980f27
d41dev <d41dev@proton.me>
2025-08-16 18:22:06 +0100

    refactor search function

--- app/src/main/java/com/fpf/smartscan/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/search/SearchViewModel.kt
@@ -56,6 +56,9 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
     var imageEmbeddings: List<Embedding> = emptyList()
     var videoEmbeddings: List<Embedding> = emptyList()
 
+    private val _mode = MutableLiveData<MediaType>(MediaType.IMAGE)
+    val mode: LiveData<MediaType> = _mode
+
     private val hasAnyImages: LiveData<Boolean> = repository.hasAnyEmbedding
     private val hasAnyVideos: LiveData<Boolean> = videoRepository.hasAnyVideoEmbeddings
     val hasIndexed = MediatorLiveData<Boolean>().apply {
@@ -84,9 +87,6 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
     private val _error = MutableLiveData<String?>()
     val error: LiveData<String?> = _error
 
-    private val _mode = MutableLiveData<MediaType>(MediaType.IMAGE)
-    val mode: LiveData<MediaType> = _mode
-
     private val _canSearchImages = MutableLiveData<Boolean>(false)
     private val _canSearchVideos = MutableLiveData<Boolean>(false)
     val canSearch = MediatorLiveData<Boolean>().apply {
@@ -102,7 +102,6 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
         addSource(_canSearchImages) { update() }
         addSource(_canSearchVideos) { update() }
     }
-
 
     private val _resultToView = MutableLiveData<Uri?>()
     val resultToView: LiveData<Uri?> = _resultToView
@@ -169,20 +168,15 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
         }
     }
 
-    fun search(n: Int, threshold: Float = 0.2f){
-        when (_mode.value) {
-            MediaType.IMAGE -> searchImages(n, imageEmbeddings, threshold)
-            MediaType.VIDEO -> searchVideos(n, videoEmbeddings, threshold)
-            null -> {}
-        }
-    }
 
-    private fun searchImages(n: Int, embeddings: List<Embedding>, threshold: Float = 0.2f) {
+    fun search(n: Int, threshold: Float = 0.2f) {
         val currentQuery = _query.value
         if (currentQuery.isNullOrBlank()) {
             _error.value = application.getString(R.string.search_error_empty_query)
             return
         }
+
+        val embeddings = if(_mode.value == MediaType.VIDEO) videoEmbeddings else imageEmbeddings
 
         if (embeddings.isEmpty()) {
             _error.value = application.getString(R.string.search_error_not_indexed)
@@ -216,68 +210,11 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
                     return@launch
                 }
 
-                val searchResultsUris = results.map { idx -> getImageUriFromId(embeddings[idx].id) }
-                val filteredSearchResultsUris = searchResultsUris.filter { uri ->
-                    canOpenUri(application, uri)
+                val searchResultsUris = if(_mode.value == MediaType.VIDEO) {
+                    results.map { idx -> getVideoUriFromId(embeddings[idx].id) }}
+                else {
+                    results.map { idx -> getImageUriFromId(embeddings[idx].id) }
                 }
-
-                if (filteredSearchResultsUris.isEmpty()) {
-                    _error.postValue(application.getString(R.string.search_error_no_results))
-                    _searchResults.postValue(emptyList())
-                    return@launch
-                }
-
-                _searchResults.postValue(filteredSearchResultsUris)
-
-            } catch (e: Exception) {
-                Log.e("SearchViewModel", "$e")
-                _error.postValue(application.getString(R.string.search_error_unknown))
-            } finally {
-                _isLoading.postValue(false)
-            }
-        }
-    }
-
-    private fun searchVideos(n: Int, embeddings: List<Embedding>, threshold: Float = 0.2f) {
-        val currentQuery = _query.value
-        if (currentQuery.isNullOrBlank()) {
-            _error.value = application.getString(R.string.search_error_empty_query)
-            return
-        }
-
-        if (embeddings.isEmpty()) {
-            _error.value = application.getString(R.string.search_error_not_indexed)
-            return
-        }
-
-        _isLoading.value = true
-        _error.value = null
-
-        viewModelScope.launch((Dispatchers.IO)) {
-            try {
-                if (embeddingsHandler == null) {
-                    embeddingsHandler = Embeddings(application.resources, ModelType.TEXT)
-                }
-                val textEmbedding = embeddingsHandler?.generateTextEmbedding(currentQuery)
-                    ?: throw IllegalArgumentException("Failed to generate text embedding.")
-
-                val similarities = getSimilarities(textEmbedding, embeddings.map { it.embeddings })
-
-                if (similarities.isEmpty()) {
-                    _error.postValue(application.getString(R.string.search_error_no_results))
-                    _searchResults.postValue(emptyList())
-                    return@launch
-                }
-
-                val results = getTopN(similarities, n, threshold)
-
-                if (results.isEmpty()) {
-                    _error.postValue(application.getString(R.string.search_error_no_results))
-                    _searchResults.postValue(emptyList())
-                    return@launch
-                }
-
-                val searchResultsUris = results.map { idx -> getVideoUriFromId(embeddings[idx].id) }
                 val filteredSearchResultsUris = searchResultsUris.filter { uri ->
                     canOpenUri(application, uri)
                 }
@@ -322,7 +259,6 @@ class SearchViewModel(private val application: Application) : AndroidViewModel(a
     fun toggleViewResult(uri: Uri?){
         _resultToView.value = uri
     }
-
 
 
     override fun onCleared() {
