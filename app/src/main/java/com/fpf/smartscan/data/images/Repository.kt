a9602cb334e19db9b124d2d4a5f78aaab972f0f3
d41dev <d41dev@proton.me>
2025-09-14 08:51:45 +0100

    remove uncessary store param

--- app/src/main/java/com/fpf/smartscan/data/images/Repository.kt
+++ app/src/main/java/com/fpf/smartscan/data/images/Repository.kt
@@ -1,7 +1,6 @@
 package com.fpf.smartscan.data.images
 
 import com.fpf.smartscansdk.core.ml.embeddings.Embedding
-import com.fpf.smartscansdk.extensions.embeddings.FileEmbeddingStore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
@@ -9,12 +8,11 @@ import kotlinx.coroutines.withContext
 class ImageEmbeddingRepository(private val dao: ImageEmbeddingDao) {
     val hasAnyEmbedding: Flow<Boolean> = dao.hasAnyImageEmbedding()
 
-    suspend fun getAllEmbeddingsWithFileSync(store: FileEmbeddingStore): List<Embedding> {
+    suspend fun getAllEmbeddingsWithFileSync(): List<Embedding> {
         val embeddings = dao.getAllEmbeddingsSync()
         val mappedEmbeddings = embeddings.map { it.toEmbedding() }
         if (mappedEmbeddings.isNotEmpty()) {
             withContext(Dispatchers.IO) {
-                store.add(mappedEmbeddings)
                 dao.deleteAll() // room db no longer needed
             }
         }
