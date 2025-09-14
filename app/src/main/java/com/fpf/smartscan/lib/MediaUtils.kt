921ca73350ab8df40d651ff5d242820f53ea630a
d41dev <d41dev@proton.me>
2025-09-14 09:45:31 +0100

    merge image and video utils into media utils

--- app/src/main/java/com/fpf/smartscan/lib/MediaUtils.kt
+++ app/src/main/java/com/fpf/smartscan/lib/MediaUtils.kt
@@ -4,10 +4,12 @@ import android.content.ContentUris
 import android.content.Context
 import android.content.Intent
 import android.graphics.*
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
 import android.util.LruCache
+import androidx.core.graphics.scale
 import androidx.documentfile.provider.DocumentFile
 import com.fpf.smartscansdk.core.ml.embeddings.clip.ClipConfig
 import com.fpf.smartscansdk.core.utils.getBitmapFromUri
@@ -103,3 +105,42 @@ fun openImageInGallery(context: Context, uri: Uri) {
     context.startActivity(intent)
 }
 
+
+fun getVideoUriFromId(id: Long): Uri {
+    return ContentUris.withAppendedId(
+        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+        id
+    )
+}
+
+suspend fun loadVideoThumbnailFromUri(
+    context: Context,
+    uri: Uri,
+    maxSize: Int
+): Bitmap? {
+    return withContext(Dispatchers.IO) {
+        BitmapCache.get(uri) ?: try {
+            val retriever = MediaMetadataRetriever()
+            retriever.setDataSource(context, uri)
+            var bitmap = retriever.getFrameAtTime(0, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
+            retriever.release()
+            if (bitmap != null) {
+                val (w, h) = getScaledDimensions(imgWith  = bitmap.width, imgHeight = bitmap.height, maxSize)
+                bitmap = bitmap.scale(w, h)
+            }
+            bitmap?.let { BitmapCache.put(uri, it) }
+            bitmap
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
+
+fun openVideoInGallery(context: Context, uri: Uri) {
+    val intent = Intent(Intent.ACTION_VIEW).apply {
+        setDataAndType(uri, "video/*")
+        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+    }
+    context.startActivity(intent)
+}
