c8df6363bffaf154d127992fd25b47d6b7525e40
d41dev <d41dev@proton.me>
2025-09-14 17:42:07 +0100

    create base index listener to avoid duplication

--- app/src/main/java/com/fpf/smartscan/lib/IndexListener.kt
+++ app/src/main/java/com/fpf/smartscan/lib/IndexListener.kt
@@ -10,18 +10,17 @@ import com.fpf.smartscansdk.core.processors.IProcessorListener
 import com.fpf.smartscansdk.core.processors.Metrics
 import com.fpf.smartscansdk.core.processors.ProcessorStatus
 
-object ImageIndexListener : IProcessorListener<Long, Embedding> {
-    const val NOTIFICATION_ID = 1002
-    const val TAG = "ImageIndexListener"
+abstract class BaseIndexListener(private val notificationId: Int, private val tag: String) : IProcessorListener<Long, Embedding> {
     private val _progress = MutableStateFlow(0f)
     val progress: StateFlow<Float> = _progress
 
-
-    private val _indexingStatus = MutableStateFlow<ProcessorStatus>(ProcessorStatus.IDLE)
+    private val _indexingStatus = MutableStateFlow(ProcessorStatus.IDLE)
     val indexingStatus: StateFlow<ProcessorStatus> = _indexingStatus
 
+    abstract val itemName: String
+
     override suspend fun onProgress(context: Context, progress: Float) {
-        if(_indexingStatus.value != ProcessorStatus.ACTIVE){
+        if (_indexingStatus.value != ProcessorStatus.ACTIVE) {
             _indexingStatus.value = ProcessorStatus.ACTIVE
         }
         _progress.value = progress
@@ -29,16 +28,14 @@ object ImageIndexListener : IProcessorListener<Long, Embedding> {
 
     override suspend fun onComplete(context: Context, metrics: Metrics.Success) {
         if (metrics.totalProcessed == 0) return
-
         try {
             _indexingStatus.value = ProcessorStatus.COMPLETE
             _progress.value = 0f
             val (minutes, seconds) = getTimeInMinutesAndSeconds(metrics.timeElapsed)
-            val notificationText = "Total images indexed: ${metrics.totalProcessed}, Time: ${minutes}m ${seconds}s"
-            showNotification(context, context.getString(R.string.notif_title_index_complete), notificationText, NOTIFICATION_ID)
-        }
-        catch (e: Exception){
-            Log.e(TAG, "Error in onComplete: ${e.message}", e)
+            val notificationText = "Total $itemName indexed: ${metrics.totalProcessed}, Time: ${minutes}m ${seconds}s"
+            showNotification(context, context.getString(R.string.notif_title_index_complete), notificationText, notificationId)
+        } catch (e: Exception) {
+            Log.e(tag, "Error in onComplete: ${e.message}", e)
         }
     }
 
@@ -46,60 +43,26 @@ object ImageIndexListener : IProcessorListener<Long, Embedding> {
         try {
             _indexingStatus.value = ProcessorStatus.FAILED
             _progress.value = 0f
-            val title = context.getString(R.string.notif_title_index_error_service, "Image")
-            val content = context.getString(R.string.notif_content_index_error_service, "image")
-            showNotification(context, title, content, NOTIFICATION_ID)
-        }
-        catch (e: Exception){
-            Log.e(TAG, "Error in onError: ${e.message}", e)
+            val title = context.getString(R.string.notif_title_index_error_service, itemName)
+            val content = context.getString(R.string.notif_content_index_error_service, itemName.lowercase())
+            showNotification(context, title, content, notificationId)
+        } catch (e: Exception) {
+            Log.e(tag, "Error in onFail: ${e.message}", e)
         }
     }
-
 }
 
+// Singletons
+object ImageIndexListener : BaseIndexListener(
+    notificationId = 1002,
+    tag = "ImageIndexListener"
+) {
+    override val itemName: String = "Image"
+}
 
-object VideoIndexListener : IProcessorListener<Long, Embedding> {
-    const val NOTIFICATION_ID = 1002
-    const val TAG = "VideoIndexListener"
-    private val _progress = MutableStateFlow(0f)
-    val progress: StateFlow<Float> = _progress
-
-    private val _indexingStatus = MutableStateFlow<ProcessorStatus>(ProcessorStatus.IDLE)
-    val indexingStatus: StateFlow<ProcessorStatus> = _indexingStatus
-
-    override suspend fun onProgress(context: Context, progress: Float) {
-        if(_indexingStatus.value != ProcessorStatus.ACTIVE){
-            _indexingStatus.value = ProcessorStatus.ACTIVE
-        }
-        _progress.value = progress
-    }
-
-    override suspend fun onComplete(context: Context, metrics: Metrics.Success) {
-        if (metrics.totalProcessed == 0) return
-
-        try {
-            _indexingStatus.value = ProcessorStatus.COMPLETE
-            _progress.value = 0f
-            val (minutes, seconds) = getTimeInMinutesAndSeconds(metrics.timeElapsed)
-            val notificationText = "Total videos indexed: ${metrics.totalProcessed}, Time: ${minutes}m ${seconds}s"
-            showNotification(context, context.getString(R.string.notif_title_index_complete), notificationText, NOTIFICATION_ID)
-        }
-        catch (e: Exception){
-            Log.e(TAG, "Error in onComplete: ${e.message}", e)
-        }
-    }
-
-    override suspend fun onFail(context: Context, metrics: Metrics.Failure) {
-        try {
-            _indexingStatus.value = ProcessorStatus.FAILED
-            _progress.value = 0f
-            val title = context.getString(R.string.notif_title_index_error_service, "Video")
-            val content = context.getString(R.string.notif_content_index_error_service, "video")
-            showNotification(context, title, content, NOTIFICATION_ID)
-        }
-        catch (e: Exception){
-            Log.e(TAG, "Error in onError: ${e.message}", e)
-        }
-    }
-
+object VideoIndexListener : BaseIndexListener(
+    notificationId = 1002,
+    tag = "VideoIndexListener"
+) {
+    override val itemName: String = "Video"
 }
