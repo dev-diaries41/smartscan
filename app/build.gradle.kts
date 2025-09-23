75f320558e78a7663017367d127936e418b89fcb
d41dev <d41dev@proton.me>
2025-09-23 13:56:13 +0100

    update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.dev-diaries41:smartscan-extensions:1.0.0")
+    implementation(libs.smartscan.extensions)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -73,7 +73,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
-//    implementation(libs.onnxruntime.android)
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
