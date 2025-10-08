4d6ee9cacfae2542a927f395cb55bc0de82f144c
d41dev <d41dev@proton.me>
2025-10-08 12:01:36 +0100

    fix: use moveHistory as single source of truth and remove job manager

--- app/src/main/java/com/fpf/smartscan/workers/ClassificationBatchWorker.kt
+++ app/src/main/java/com/fpf/smartscan/workers/ClassificationBatchWorker.kt
@@ -8,13 +8,14 @@ import androidx.core.content.edit
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.fpf.smartscan.R
+import com.fpf.smartscan.data.movehistory.MoveHistoryDatabase
+import com.fpf.smartscan.data.movehistory.MoveHistoryRepository
 import com.fpf.smartscan.data.prototypes.PrototypeEmbeddingDatabase
 import com.fpf.smartscan.data.prototypes.PrototypeEmbeddingRepository
 import com.fpf.smartscan.data.prototypes.toEmbedding
 import com.fpf.smartscan.data.scans.AppDatabase
 import com.fpf.smartscan.data.scans.ScanData
 import com.fpf.smartscan.data.scans.ScanDataRepository
-import com.fpf.smartscan.lib.JobManager
 import com.fpf.smartscan.lib.OrganiserListener
 import com.fpf.smartscan.lib.getTimeInMinutesAndSeconds
 import com.fpf.smartscan.lib.readUriListFromFile
@@ -25,7 +26,6 @@ import com.fpf.smartscansdk.core.processors.Metrics
 import com.fpf.smartscansdk.extensions.organisers.Organiser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-
 
 /**
  * Batch worker that reads a persisted file containing all image URIs,
@@ -38,11 +38,8 @@ class ClassificationBatchWorker(context: Context, workerParams: WorkerParameters
     companion object {
         const val TAG = "ClassificationBatchWorker"
         private const val JOB_NAME = ClassificationWorker.JOB_NAME
+        private const val ERROR_COUNT = "ERROR_COUNT"
     }
-
-    private val jobManager = JobManager.getInstance(context)
-    private var startTime: Long = 0L
-
     private val scanId = inputData.getInt("SCAN_ID", -1)
     private val batchIndex = inputData.getInt("BATCH_INDEX", -1)
     private val batchSize = inputData.getInt("BATCH_SIZE", -1)
@@ -51,13 +48,15 @@ class ClassificationBatchWorker(context: Context, workerParams: WorkerParameters
     private val imageUriFilePath = inputData.getString("IMAGE_URI_FILE") ?: ""
     private val threshold = inputData.getFloat("THRESHOLD", 0.4f)
     private val confidenceMargin = inputData.getFloat("CONFIDENCE_MARGIN", 0.03f)
-    private val repository = ScanDataRepository(AppDatabase.getDatabase(applicationContext as Application).scanDataDao())
-    private val prototypeRepository: PrototypeEmbeddingRepository = PrototypeEmbeddingRepository(PrototypeEmbeddingDatabase.getDatabase(context.applicationContext as Application).prototypeEmbeddingDao())
+    private val scanDataRepository = ScanDataRepository(AppDatabase.getDatabase(applicationContext as Application).scanDataDao())
+    private val prototypeRepository = PrototypeEmbeddingRepository(PrototypeEmbeddingDatabase.getDatabase(context.applicationContext as Application).prototypeEmbeddingDao())
+    private val moveHistoryRepository = MoveHistoryRepository(MoveHistoryDatabase.getDatabase(context.applicationContext as Application).moveHistoryDao())
+    private val sharedPrefs = context.getSharedPreferences(JOB_NAME, Context.MODE_PRIVATE)
+    private var errorCount = getErrorCount()
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         val embeddingHandler = ClipImageEmbedder(applicationContext.resources, ResourceId(R.raw.image_encoder_quant_int8))
         val prototypes = prototypeRepository.getAllEmbeddingsSync().map{it.toEmbedding()}
-
         val organiser = Organiser(applicationContext as Application, embeddingHandler, scanId=scanId, listener = OrganiserListener, prototypeList = prototypes, threshold = threshold, confidenceMargin = confidenceMargin)
 
         try {
@@ -72,20 +71,11 @@ class ClassificationBatchWorker(context: Context, workerParams: WorkerParameters
 
             val batchUriList = getBatchUris()
 
-            Log.i(TAG, "Processing classification batch $batchIndex with ${batchUriList.size} images.")
+            Log.i(TAG, "Processing batch $batchIndex with ${batchUriList.size} images.")
             val results = organiser.run(batchUriList)
-            val end = System.currentTimeMillis()
 
             when(results){
-                is Metrics.Success -> {
-                    jobManager.onComplete(
-                        jobName = JOB_NAME,
-                        startTime = end - results.timeElapsed,
-                        finishTime = end,
-                        processedCount = results.totalProcessed -  OrganiserListener.failedMoveCount
-                    )
-//                    Log.d(TAG, "FAILED MOVE COUNT FOR BATCH $batchIndex: ${OrganiserListener.failedMoveCount}")
-                }
+                is Metrics.Success -> {}
                 // Let worker handle batch errors
                 is Metrics.Failure -> {
                     throw results.error
@@ -94,40 +84,30 @@ class ClassificationBatchWorker(context: Context, workerParams: WorkerParameters
 
             if (isLastBatch) {
                 onAllJobsComplete()
-                jobManager.clearJobs(JOB_NAME)
             }
 
+            resetErrorCount()
             return@withContext Result.success()
         } catch (e: Exception) {
-            val failTime = System.currentTimeMillis()
-            Log.e(TAG, "Error processing classification batch: ${e.message}", e)
-
-            jobManager.onError(
-                jobName = JOB_NAME,
-                startTime = startTime,
-                finishTime = failTime,
-                processedCount = 0
-            )
-            val results = jobManager.getJobResults(JOB_NAME)
-            if(results.errorCount >= 3){
-                jobManager.clearJobs(JOB_NAME)
+            Log.e(TAG, "Error processing batch $batchIndex (error count $errorCount): ${e.message}", e)
+            incrementErrorCount()
+            if(errorCount >= 3){
+                resetErrorCount()
                 // Temporary workaround to avoid modifying db schema:
-                // If totalProcessedCount > 0 it indicates some batches were successful.
                 // ERROR_RESULT (-1) is used to indicate a failure with no reliable result.
-                val count = if(results.totalProcessedCount > 0) results.totalProcessedCount else ScanData.ERROR_RESULT
-                repository.update(scanId, count, System.currentTimeMillis())
+                val moveHistory = moveHistoryRepository.getMoveHistory(scanId)
+                val count = if(moveHistory.isNotEmpty()) moveHistory.size else ScanData.ERROR_RESULT
+                scanDataRepository.update(scanId, count, System.currentTimeMillis())
                 // Do not continually retry unlike indexing, because this does not have a super critical impact on usability
                 showNotification(applicationContext, applicationContext.getString(R.string.notif_title_smart_scan_issue), applicationContext.getString(R.string.notif_title_smart_scan_issue_description), 1003)
                 return@withContext Result.failure()
             }
-
             return@withContext Result.retry()
         } finally {
             organiser.close()
-            saveLastUsedDestinations(applicationContext, prototypes.map { it.id })
+            saveLastUsedDestinations(prototypes.map { it.id })
         }
     }
-
     private fun getBatchUris(): List<Uri>{
         val uriList = readUriListFromFile(imageUriFilePath)
         val startIndex = batchIndex * batchSize
@@ -135,26 +115,35 @@ class ClassificationBatchWorker(context: Context, workerParams: WorkerParameters
         val batchUriList = uriList.subList(startIndex, endIndex)
         return batchUriList
     }
-
-    private fun saveLastUsedDestinations(context: Context, files: List<String>) {
-        val prefs = context.getSharedPreferences(ClassificationWorker.JOB_NAME, Context.MODE_PRIVATE)
-        prefs.edit() {
-            putStringSet(Organiser.PREF_KEY_LAST_USED_CLASSIFICATION_DIRS, files.toSet())
-        }
+    private fun saveLastUsedDestinations(files: List<String>) {
+        sharedPrefs.edit { putStringSet(Organiser.PREF_KEY_LAST_USED_CLASSIFICATION_DIRS, files.toSet()) }
     }
-
+    private fun resetErrorCount() {
+        errorCount = 0
+        sharedPrefs.edit { putInt(ERROR_COUNT, errorCount) }
+    }
+    private fun incrementErrorCount() {
+        ++errorCount
+        sharedPrefs.edit { putInt(ERROR_COUNT, errorCount) }
+    }
+    private fun getErrorCount(): Int {
+        return sharedPrefs.getInt(ERROR_COUNT, 0)
+    }
     private suspend fun onAllJobsComplete(){
-        val results = jobManager.getJobResults(JOB_NAME)
         try {
-            if (results.totalProcessedCount == 0){
-                repository.delete(scanId)
+            val moveHistory = moveHistoryRepository.getMoveHistory(scanId)
+            if (moveHistory.isEmpty()){
+                scanDataRepository.delete(scanId)
                 return
             }
 
-            repository.update(scanId, results.totalProcessedCount, System.currentTimeMillis())
-            val totalProcessingTime = results.finishTime - results.startTime
+            val startTime = moveHistory.minOf { it.date }
+            val finishTime = moveHistory.maxOf { it.date }
+
+            scanDataRepository.update(scanId, moveHistory.size, System.currentTimeMillis())
+            val totalProcessingTime = finishTime - startTime
             val (minutes, seconds) = getTimeInMinutesAndSeconds(totalProcessingTime)
-            val notificationText = "Total images moved: ${results.totalProcessedCount}, Time: ${minutes}m ${seconds}s"
+            val notificationText = "Total images moved: ${moveHistory.size}, Time: ${minutes}m ${seconds}s"
 
             showNotification(applicationContext, applicationContext.getString(R.string.notif_title_auto_organisation_complete), notificationText, 1003)
         }
