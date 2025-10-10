ae6ad19e19bc9c8e665415e95393b42d7458b1aa
d41dev <d41dev@proton.me>
2025-10-10 20:57:44 +0100

    update: imports

--- app/src/main/java/com/fpf/smartscan/lib/Models.kt
+++ app/src/main/java/com/fpf/smartscan/lib/Models.kt
@@ -3,11 +3,11 @@ package com.fpf.smartscan.lib
 import android.content.Context
 import com.fpf.smartscan.R
 import com.fpf.smartscan.data.DownloadableModel
-import com.fpf.smartscan.constants.SmartScanModelTypes
+import com.fpf.smartscan.data.SmartScanModelType
 
 fun getModels(context: Context): List<DownloadableModel>{
     val facialRecognitionModel = DownloadableModel(
-        type = SmartScanModelTypes.FACE,
+        type = SmartScanModelType.FACE,
         name = context.getString(R.string.facial_recognition_model_name),
         url = context.getString(R.string.inception_resnet_v1_model_url),
     )
