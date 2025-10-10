610584b8ab4c8f0770e964e855b53c3f141fe924
d41dev <d41dev@proton.me>
2025-10-10 17:01:29 +0100

    update: add Models constants for file paths and model types

---
+++ app/src/main/java/com/fpf/smartscan/constants/Models.kt
@@ -0,0 +1,17 @@
+package com.fpf.smartscan.constants
+
+object SmartScanModelTypes {
+    const val FACE = "facial"
+    const val OBJECTS = "object"
+    const val IMAGE_ENCODER = "image_encoder"
+    const val TEXT_ENCODER = "text_encoder"
+}
+
+const val MODEL_DIR = "models"
+
+object ModelPaths {
+    const val FACE = "$MODEL_DIR/${SmartScanModelTypes.FACE}.zip"
+    const val OBJECTS =  "$MODEL_DIR/${SmartScanModelTypes.OBJECTS}.onnx"
+    const val IMAGE_ENCODER = "$MODEL_DIR/${SmartScanModelTypes.IMAGE_ENCODER}.onnx"
+    const val TEXT_ENCODER = "$MODEL_DIR/${SmartScanModelTypes.TEXT_ENCODER}.onnx"
+}
