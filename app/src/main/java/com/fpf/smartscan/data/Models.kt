8342f9b2f74fb573507ef7ef980bd60d4bde5eca
d41dev <d41dev@proton.me>
2025-10-10 17:01:57 +0100

    update: move SmartScanModelTypes to constants

--- app/src/main/java/com/fpf/smartscan/data/Models.kt
+++ app/src/main/java/com/fpf/smartscan/data/Models.kt
@@ -1,12 +1,5 @@
 package com.fpf.smartscan.data
 
-object SmartScanModelTypes {
-    const val FACE = "facial"
-    const val OBJECTS = "object"
-    const val IMAGE_ENCODER = "image encoder"
-    const val TEXT_ENCODER = "text encoder"
-}
-
 data class DownloadableModel(
     val type: String,
     val name: String,
