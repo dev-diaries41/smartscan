d38794d5d67bbce5eb6e4da7f437ad9746f2df09
d41dev <d41dev@proton.me>
2025-10-10 15:44:25 +0100

    upate: add wrapper function getModels

---
+++ app/src/main/java/com/fpf/smartscan/lib/Models.kt
@@ -0,0 +1,15 @@
+package com.fpf.smartscan.lib
+
+import android.content.Context
+import com.fpf.smartscan.R
+import com.fpf.smartscan.data.DownloadableModel
+import com.fpf.smartscan.data.SmartScanModelTypes
+
+fun getModels(context: Context): List<DownloadableModel>{
+    val facialRecognitionModel = DownloadableModel(
+        type = SmartScanModelTypes.FACE,
+        name = context.getString(R.string.facial_recognition_model_name),
+        url = context.getString(R.string.inception_resnet_v1_model_url),
+    )
+    return listOf(facialRecognitionModel)
+}
