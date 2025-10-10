f1eea9dd889206960d6879c6a4516eeeced6dbf2
d41dev <d41dev@proton.me>
2025-10-10 08:49:20 +0100

    update: add data structures for downloadable models

---
+++ app/src/main/java/com/fpf/smartscan/data/Models.kt
@@ -0,0 +1,14 @@
+package com.fpf.smartscan.data
+
+object SmartScanModelTypes {
+    const val FACE = "facial"
+    const val OBJECTS = "object"
+    const val IMAGE_ENCODER = "image encoder"
+    const val TEXT_ENCODER = "text encoder"
+}
+
+data class DownloadableModel(
+    val type: String,
+    val name: String,
+    val url: String
+)
