0a9e206a98c26369c551ad9ee6d2e86920887fee
d41dev <d41dev@proton.me>
2025-10-11 00:42:27 +0100

    update: add deleteModel fun and update validation of imported model in getImportedModels

--- app/src/main/java/com/fpf/smartscan/lib/Models.kt
+++ app/src/main/java/com/fpf/smartscan/lib/Models.kt
@@ -2,9 +2,9 @@ package com.fpf.smartscan.lib
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import com.fpf.smartscan.R
 import com.fpf.smartscan.constants.modelPathsMap
-import com.fpf.smartscan.constants.smartScanModelTypeOptions
 import com.fpf.smartscan.data.DownloadableModel
 import com.fpf.smartscan.data.ImportedModel
 import com.fpf.smartscan.data.SmartScanModelType
@@ -31,9 +31,37 @@ fun getImportedModels(context: Context): List<ImportedModel>{
             name = context.getString(R.string.facial_recognition_model_name),
             dependentModelPaths = modelPathsMap[SmartScanModelType.FACE]!!.dependentModelPaths,
         )
-        val importedModels = mutableListOf(facialRecognitionModel).filter { File(context.filesDir, modelPathsMap[it.type]!!.path).exists() }
+        val importedModels = mutableListOf(facialRecognitionModel).filter {  isImported(context, it)}
         return importedModels
-    }catch (e: Exception){return emptyList()}
+    }catch (e: Exception){
+        Log.e("getImportedModels", "Error getting imported models: ${e.message}")
+        return emptyList()
+    }
+}
+
+private fun isImported(context: Context, model: ImportedModel): Boolean{
+    if(File(context.filesDir, modelPathsMap[model.type]!!.path).exists()) {
+        return true
+    }else if(model.dependentModelPaths.isNotEmpty()){
+        return model.dependentModelPaths.any{File(context.filesDir, it).exists()}
+    }
+    return false
+}
+
+fun deleteModel(context: Context, model: ImportedModel): Boolean{
+    try {
+        val modelPathInfo = modelPathsMap[model.type]!!
+        val file = File(context.filesDir, modelPathInfo.path )
+        file.delete()
+
+        if(modelPathInfo.dependentModelPaths.isNotEmpty()){
+            modelPathInfo.dependentModelPaths.forEach {File(context.filesDir, it ).delete()  }
+        }
+        return true
+    }catch (e: Exception){
+        Log.e("deleteModel", "Error deleting model: ${e.message}")
+        return false
+    }
 }
 
 suspend fun importModel(context: Context, uri: Uri, type: SmartScanModelType) = withContext(Dispatchers.IO){
@@ -61,4 +89,5 @@ suspend fun importModel(context: Context, uri: Uri, type: SmartScanModelType) = 
             }
         }
     }
+    outputFile.delete()
 }
