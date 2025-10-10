7cf08ddf1ec19cd1839314c5a912b6ccd1af82c7
d41dev <d41dev@proton.me>
2025-10-10 20:46:15 +0100

    update: use enum class SmartScanModelType

--- app/src/main/java/com/fpf/smartscan/data/Models.kt
+++ app/src/main/java/com/fpf/smartscan/data/Models.kt
@@ -1,7 +1,14 @@
 package com.fpf.smartscan.data
 
+enum class SmartScanModelType(val fileName: String) {
+    FACE("facial"),
+    OBJECTS("object"),
+    IMAGE_ENCODER("image_encoder"),
+    TEXT_ENCODER("text_encoder")
+}
+
 data class DownloadableModel(
-    val type: String,
+    val type: SmartScanModelType,
     val name: String,
     val url: String,
 )
