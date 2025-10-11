894184b2fe8d91b5b4b7f343963428eb6045ab75
d41dev <d41dev@proton.me>
2025-10-11 09:06:06 +0100

    update: replace APplicaton with context and use context.applicationContext

--- app/src/main/java/com/fpf/smartscan/services/IndexServiceManager.kt
+++ app/src/main/java/com/fpf/smartscan/services/IndexServiceManager.kt
@@ -1,19 +1,19 @@
 package com.fpf.smartscan.services
 
-import android.app.Application
+import android.content.Context
 import android.content.Intent
 import com.fpf.smartscan.lib.isServiceRunning
 
-fun startIndexing(application: Application, mediaType: String) {
-    Intent(application, MediaIndexForegroundService::class.java)
+fun startIndexing(context: Context, mediaType: String) {
+    Intent(context.applicationContext, MediaIndexForegroundService::class.java)
         .putExtra(MediaIndexForegroundService.EXTRA_MEDIA_TYPE, mediaType)
-        .also { intent -> application.startForegroundService(intent) }
+        .also { intent -> context.applicationContext.startForegroundService(intent) }
 }
 
-fun refreshIndex(application: Application, mediaType: String) {
-    val running = isServiceRunning(application, MediaIndexForegroundService::class.java)
+fun refreshIndex(context: Context, mediaType: String) {
+    val running = isServiceRunning(context.applicationContext, MediaIndexForegroundService::class.java)
     if(running){
-        application.stopService(Intent(application, MediaIndexForegroundService::class.java))
+        context.applicationContext.stopService(Intent(context.applicationContext, MediaIndexForegroundService::class.java))
     }
-    startIndexing(application, mediaType)
+    startIndexing(context.applicationContext, mediaType)
 }
