9d7ccecc898a1b979d74a4e32e10d0494e9ea634
d41dev <d41dev@proton.me>
2025-10-11 09:19:54 +0100

    update: add generic cancelWorker function

--- app/src/main/java/com/fpf/smartscan/lib/Utils.kt
+++ app/src/main/java/com/fpf/smartscan/lib/Utils.kt
@@ -12,14 +12,12 @@ import androidx.core.content.ContextCompat
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.fpf.smartscan.R
-import com.fpf.smartscan.data.scans.ScanData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import kotlin.collections.any
-import kotlin.random.Random
 
 fun toDateString(timestamp: Long): String {
     val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
@@ -30,20 +28,6 @@ fun getTimeInMinutesAndSeconds(milliseconds: Long): Pair<Long, Long> {
     val seconds = milliseconds / 1000
     val minutes = seconds / 60
     return Pair(minutes, seconds % 60)
-}
-
-fun generatePlaceholderScanHistory(n: Int): List<ScanData> {
-    val generatedItems = mutableListOf<ScanData>()
-
-    for (i in 0 until n) {
-        val date = System.currentTimeMillis() - (i * 1000 * 60 * 60 * 24)
-        val item = ScanData(
-            date = date,
-            result = i +  Random.nextInt(1, 20)
-        )
-        generatedItems.add(item)
-    }
-    return generatedItems
 }
 
 fun showNotification(context: Context, title: String, text: String, id: Int = 1001) {
@@ -86,3 +70,8 @@ suspend fun isWorkScheduled(context: Context, workName: String): Boolean {
     }
 }
 
+fun cancelWorker(context: Context, uniqueWorkName: String?, tag: String?){
+    val workManager = WorkManager.getInstance(context.applicationContext)
+    uniqueWorkName?.let{workManager.cancelUniqueWork(it)}
+    tag?.let{workManager.cancelAllWorkByTag(it)}
+}
