6256e55f37e8704185f95c7bb4961d1396d4a456
d41dev <d41dev@proton.me>
2025-10-11 19:47:26 +0100

    update: import downloablemodelcard and use adjust layout

--- app/src/main/java/com/fpf/smartscan/ui/components/models/ModelsList.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/models/ModelsList.kt
@@ -1,35 +1,8 @@
 package com.fpf.smartscan.ui.components.models
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Download
-import androidx.compose.material.icons.filled.Memory
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.fpf.smartscan.R
-import com.fpf.smartscan.constants.smartScanModelTypeOptions
 import com.fpf.smartscan.data.DownloadableModel
 
 @Composable
@@ -37,92 +10,10 @@ fun ModelsList(
     models: List<DownloadableModel>,
     onDownload: (url: String) -> Unit,
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        models.forEach { item ->
-            ModelCard(data = item, onDownload = onDownload)
-        }
-    }
-}
-
-@Composable
-fun ModelCard(
-    data: DownloadableModel,
-    onDownload: (url: String) -> Unit,
-) {
-    var isDownloadAlertVisible by remember { mutableStateOf(false) }
-
-
-    if(isDownloadAlertVisible){
-        AlertDialog(
-            onDismissRequest = { },
-            title = { Text(stringResource(R.string.download_model_alert_title)) },
-            text = { Text(stringResource(R.string.download_model_alert_description)) },
-            dismissButton = {
-                TextButton(onClick = {
-                    isDownloadAlertVisible = false
-                }) {
-                    Text("Cancel")
-                }
-            },
-            confirmButton = {
-                TextButton(onClick = {
-                    isDownloadAlertVisible = false
-                    onDownload(data.url)
-                }) {
-                    Text("OK")
-                }
-            }
-        )
-    }
-
-    Card(
-        elevation = CardDefaults.cardElevation(4.dp),
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-    ) {
-        Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.Top
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Memory,
-                    contentDescription = "Model icon",
-                    modifier = Modifier.padding(end = 16.dp).size(32.dp),
-                    tint = MaterialTheme.colorScheme.primary
-                )
-                Column(
-                    modifier = Modifier.weight(1f),
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(
-                        text = data.name,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    Text(
-                        text = "Type: ${smartScanModelTypeOptions[data.type]}",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.alpha(0.8f)
-                    )
-
-                }
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                Button(
-                    onClick = { isDownloadAlertVisible = true }
-                ) {
-                    Icon(
-                        Icons.Default.Download,
-                        contentDescription = "Download icon",
-                        modifier = Modifier.padding(end = 4.dp)
-                    )
-                    Text(text = "Download")
-                }
-            }
-        }
+    LazyColumn{
+        items(
+            items = models,
+            key = { it.name }
+        ) { item -> DownloadableModelCard(data = item, onDownload = onDownload) }
     }
 }
