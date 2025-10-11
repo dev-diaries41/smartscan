3be0e85aef459950881327eb2fb102218c3d8b74
d41dev <d41dev@proton.me>
2025-10-11 21:14:16 +0100

    update: pass models and onDelete

--- app/src/main/java/com/fpf/smartscan/ui/components/models/ModelManager.kt
+++ app/src/main/java/com/fpf/smartscan/ui/components/models/ModelManager.kt
@@ -1,57 +1,35 @@
-package com.fpf.smartscan.ui.components
+package com.fpf.smartscan.ui.components.models
 
 import android.content.Intent
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Memory
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.fpf.smartscan.R
-import com.fpf.smartscan.constants.smartScanModelTypeOptions
 import com.fpf.smartscan.data.ImportedModel
-import com.fpf.smartscan.data.SmartScanModelType
-import com.fpf.smartscan.lib.deleteModel
-
 
 @Composable
 fun ModelManager(
-    importedModels: List<ImportedModel>,
+    models: List<ImportedModel>,
     description: String? = null,
-    onImport: (uri: Uri) -> Unit
+    onImport: (uri: Uri) -> Unit,
+    onDelete: (model: ImportedModel) -> Unit
 ) {
-    var models by remember { mutableStateOf(importedModels) }
     val context = LocalContext.current
     val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
         uri?.let { selectedUri ->
@@ -88,105 +66,15 @@ fun ModelManager(
             if (models.isEmpty()) {
                 Text(
                     text = "No models to manage.",
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.titleLarge,
                     modifier = Modifier.alpha(0.5f).padding(vertical = 16.dp),
                 )
             } else {
-                Card(
-                    modifier = Modifier.fillMaxWidth(),
-                    elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    colors = CardDefaults.cardColors(Color.Transparent)
-                ) {
-                    Column(modifier = Modifier.padding(vertical = 16.dp)) {
-                        models.forEach { model ->
-                            ImportedModelCard(
-                                data = model,
-                                onDelete = { models - model })
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun ImportedModelCard(
-    data: ImportedModel,
-    onDelete: () -> Unit
-) {
-    var isDeleteAlertVisible by remember { mutableStateOf(false) }
-    val context = LocalContext.current
-
-    if(isDeleteAlertVisible){
-        AlertDialog(
-            onDismissRequest = { },
-            title = { Text(stringResource(R.string.delete_model_alert_title)) },
-            text = { Text(stringResource(R.string.delete_model_alert_description)) },
-            dismissButton = {
-                TextButton(onClick = {
-                    isDeleteAlertVisible = false
-                }) {
-                    Text("Cancel")
-                }
-            },
-            confirmButton = {
-                TextButton(onClick = {
-                    isDeleteAlertVisible = false
-                    deleteModel(context, data)
-                    onDelete()
-                }) {
-                    Text("OK")
-                }
-            }
-        )
-    }
-
-    Card(
-        elevation = CardDefaults.cardElevation(4.dp),
-        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-    ) {
-        Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.Top
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Memory,
-                    contentDescription = "Model icon",
-                    modifier = Modifier.padding(end = 16.dp).size(32.dp),
-                    tint = MaterialTheme.colorScheme.primary
-                )
-                Column(
-                    modifier = Modifier.weight(1f),
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    Text(
-                        text = data.name,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    Text(
-                        text = "Type: ${smartScanModelTypeOptions[data.type]}",
-                        style = MaterialTheme.typography.bodyMedium,
-                        modifier = Modifier.alpha(0.8f)
-                    )
-
-                }
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                Button (
-                    onClick = { isDeleteAlertVisible = true }
-                ) {
-                    Icon(
-                        Icons.Default.Delete,
-                        contentDescription = "Delete icon",
-                        modifier = Modifier.padding(end = 4.dp)
-                    )
-                    Text(text = "Delete")
+                LazyColumn{
+                    items(
+                        items = models,
+                        key = { it.name }
+                    ) { item -> ImportedModelCard(data = item, onDelete = { onDelete(item)})}
                 }
             }
         }
