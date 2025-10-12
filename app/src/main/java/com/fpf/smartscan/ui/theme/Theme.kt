79ac4bef46b910d1a09252f0be57db9d0c04d77a
d41dev <d41dev@proton.me>
2025-10-12 22:18:08 +0100

    update custom palettes and an param to MyAppTheme to select color scheme

--- app/src/main/java/com/fpf/smartscan/ui/theme/Theme.kt
+++ app/src/main/java/com/fpf/smartscan/ui/theme/Theme.kt
@@ -4,36 +4,34 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-// Color scheme for light mode
+
+enum class ColorSchemeType { DEFAULT, SMARTSCAN }
+
 private val LightColorPalette = lightColorScheme(
     primary = Peach,
     onPrimary = Color.White,
-    secondary = Teal200,
-    onSecondary = Color.Black,
-    background = Color.White,  // Use your custom background color
-    surface = Color.White,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
+    primaryContainer = Peach.copy(alpha = 0.5f),
+    onPrimaryContainer = Color.Black
 )
 
-// Color scheme for dark mode
 private val DarkColorPalette = darkColorScheme(
     primary = Peach,
     onPrimary = Color.Black,
-    secondary = Teal200,
-    onSecondary = Color.Black,
-    background = Color.Black,  // Define background for dark mode
-    surface = Color.Black,
-    onBackground = Color.White,
-    onSurface = Color.White,
+    primaryContainer = Peach.copy(alpha = 0.5f),
+    onPrimaryContainer = Color.White
 )
+
 
 @Composable
 fun MyAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    colorSchemeType: ColorSchemeType = ColorSchemeType.SMARTSCAN,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+    val colors = when (colorSchemeType) {
+        ColorSchemeType.DEFAULT -> if (darkTheme) darkColorScheme() else lightColorScheme()
+        ColorSchemeType.SMARTSCAN -> if (darkTheme) DarkColorPalette else LightColorPalette
+    }
 
     MaterialTheme(
         colorScheme = colors,
