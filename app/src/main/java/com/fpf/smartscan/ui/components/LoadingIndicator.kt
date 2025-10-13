4ba5b7f2e39bc3ad376d941bcb4f9e50c0c08686
d41dev <d41dev@proton.me>
2025-10-13 10:53:30 +0100

    update: add generic loading indicator

---
+++ app/src/main/java/com/fpf/smartscan/ui/components/LoadingIndicator.kt
@@ -0,0 +1,44 @@
+package com.fpf.smartscan.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingIndicator(
+    isLoading: Boolean,
+    size: Int,
+    color: Color = ProgressIndicatorDefaults.circularColor,
+    strokeWidth: Dp = ProgressIndicatorDefaults.CircularStrokeWidth,
+){
+    AnimatedVisibility(
+        visible = isLoading,
+        enter = fadeIn(animationSpec = tween(durationMillis = 500)) + expandVertically(),
+        exit = fadeOut(animationSpec = tween(durationMillis = 500)) + shrinkVertically()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator(
+                color = color,
+                modifier = Modifier.size(size.dp),
+                strokeWidth = strokeWidth
+            )
+        }
+    }
+}
