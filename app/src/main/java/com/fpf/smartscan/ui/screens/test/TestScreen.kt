a11f3f62b0ccb94e2203a228101f27b8a0792c75
d41dev <d41dev@proton.me>
2025-10-13 10:18:56 +0100

    update: use new uploader and pass theshold and conf margin to classify func

--- app/src/main/java/com/fpf/smartscan/ui/screens/test/TestScreen.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/test/TestScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.fpf.smartscan.ui.components.ImageUploader
+import com.fpf.smartscan.data.prototypes.toEmbedding
+import com.fpf.smartscan.ui.components.NewImageUploader
 import com.fpf.smartscan.ui.screens.settings.SettingsViewModel
 
 
@@ -18,44 +20,34 @@ fun TestScreen(viewModel: TestViewModel = viewModel(), settingsViewModel: Settin
     val context = LocalContext.current
     val imageUri by viewModel.imageUri.collectAsState(null)
     val inferenceResult by viewModel.predictedClass.collectAsState(null)
-    val prototypes by settingsViewModel.prototypeList.collectAsState(emptyList())
+    val prototypesEntities by settingsViewModel.prototypeList.collectAsState()
+    val appSettings by settingsViewModel.appSettings.collectAsState()
+    val classPrototypes = prototypesEntities.map { it.toEmbedding() }
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+    Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.fillMaxSize().padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                ImageUploader(
-                    imageUri = imageUri,
+                NewImageUploader(
+                    uri = imageUri,
                     onImageSelected = { uri ->
                         viewModel.updateImageUri(uri)
                         if (uri == null) {
                             viewModel.clearInferenceResult()
                         }
+                    },
+                    placeholderText = {
+                        Text(text = "Upload image",
+                            style = MaterialTheme.typography.titleMedium,
+                            modifier = Modifier.alpha(0.8f).padding(8.dp),
+                        )
                     }
                 )
-
                 if (imageUri != null) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(
-                        onClick = {
-                            viewModel.inference(context, prototypes)
-                        },
-                    ) {
-                        Text("Classify Image")
-                    }
+                    Button(modifier = Modifier.padding(vertical = 16.dp),
+                        onClick = { viewModel.inference(context, classPrototypes, threshold = appSettings.organiserSimilarityThreshold, confidenceMargin = appSettings.organiserConfMargin) },
+                    ) { Text("Classify Image") }
                 }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                inferenceResult?.let {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(text = "Result: $it")
-                }
+                inferenceResult?.let { Text(text = "Result: $it", modifier = Modifier.padding(vertical = 16.dp)) }
             }
         }
     }
