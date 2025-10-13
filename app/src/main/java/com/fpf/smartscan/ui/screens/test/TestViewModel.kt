24dab446bf9bfb00074e51c189856e6a4c4450a5
d41dev <d41dev@proton.me>
2025-10-13 10:22:03 +0100

    update: only initilaise model in inference if not already initialised

--- app/src/main/java/com/fpf/smartscan/ui/screens/test/TestViewModel.kt
+++ app/src/main/java/com/fpf/smartscan/ui/screens/test/TestViewModel.kt
@@ -9,12 +9,11 @@ import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.fpf.smartscan.data.prototypes.PrototypeEmbeddingEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.fpf.smartscan.R
-import com.fpf.smartscan.data.prototypes.toEmbedding
 import com.fpf.smartscansdk.core.ml.embeddings.ClassificationResult
+import com.fpf.smartscansdk.core.ml.embeddings.PrototypeEmbedding
 import com.fpf.smartscansdk.core.ml.embeddings.classify
 import com.fpf.smartscansdk.core.ml.embeddings.clip.ClipConfig
 import com.fpf.smartscansdk.core.ml.embeddings.clip.ClipImageEmbedder
@@ -32,12 +31,6 @@ class TestViewModel(application: Application) : AndroidViewModel(application){
     private val _imageUri = MutableStateFlow<Uri?>(null)
     val imageUri: StateFlow<Uri?> = _imageUri
 
-    init {
-        viewModelScope.launch(Dispatchers.Default) {
-            embeddingsHandler.initialize()
-        }
-    }
-
     fun updateImageUri(uri: Uri?) {
         _imageUri.value = uri
     }
@@ -46,19 +39,19 @@ class TestViewModel(application: Application) : AndroidViewModel(application){
         _predictedClass.value = null
     }
 
-    fun inference(context: Context, prototypeEmbeddings:  List<PrototypeEmbeddingEntity>) {
-        if (prototypeEmbeddings.isEmpty()) {
+    fun inference(context: Context, classPrototypes: List<PrototypeEmbedding>, threshold: Float, confidenceMargin: Float) {
+        if (classPrototypes.isEmpty()) {
             Toast.makeText(context, context.getString(R.string.test_no_prototype_embeddings), Toast.LENGTH_SHORT).show()
             return
         }
 
         viewModelScope.launch(Dispatchers.IO) {
             try {
+                if(!embeddingsHandler.isInitialized()) embeddingsHandler.initialize()
                 val uri = _imageUri.value ?: return@launch
                 val bitmap = getBitmapFromUri(context, uri, ClipConfig.CLIP_EMBEDDING_LENGTH)
                 val imageEmbedding = embeddingsHandler.embed(bitmap)
-                val classResult =
-                    classify(imageEmbedding, prototypeEmbeddings.map { it.toEmbedding() })
+                val classResult = classify(imageEmbedding, classPrototypes, threshold = threshold, confidenceMargin = confidenceMargin)
 
                 when (classResult){
                     is ClassificationResult.Failure -> {
