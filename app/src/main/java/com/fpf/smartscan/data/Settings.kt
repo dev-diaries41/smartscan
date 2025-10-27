56667fd38046508832edf0b8ebf09fe663488db2
d41dev <d41dev@proton.me>
2025-10-27 07:55:42 +0000

    update: remove auto-organisation related settings

--- app/src/main/java/com/fpf/smartscan/data/Settings.kt
+++ app/src/main/java/com/fpf/smartscan/data/Settings.kt
@@ -6,14 +6,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppSettings(
-    val enableScan: Boolean = false,
-    val targetDirectories: List<String> = emptyList(),
-    val frequency: String = "1 Day",
-    val destinationDirectories: List<String> = emptyList(),
     val similarityThreshold: Float = 0.2f,
     val indexFrequency: String = "1 Week",
-    val organiserSimilarityThreshold: Float = 0.4f,
-    val organiserConfMargin: Float = 0.03f,
     val searchableImageDirectories: List<String> = emptyList(),
     val searchableVideoDirectories: List<String> = emptyList(),
     val theme: ThemeMode = ThemeMode.SYSTEM,
