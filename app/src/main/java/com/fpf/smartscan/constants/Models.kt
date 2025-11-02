6915c4ac464c9caa43fce65aa813de57b004c085
d41dev <d41dev@proton.me>
2025-11-02 09:29:19 +0000

    update: use ModelInfo

--- app/src/main/java/com/fpf/smartscan/constants/Models.kt
+++ app/src/main/java/com/fpf/smartscan/constants/Models.kt
@@ -1,6 +1,6 @@
 package com.fpf.smartscan.constants
 
-import com.fpf.smartscan.data.ModelPathInfo
+import com.fpf.smartscan.data.ModelInfo
 import com.fpf.smartscan.data.SmartScanModelType
 
 val smartScanModelTypeOptions = mapOf (
@@ -10,17 +10,22 @@ val smartScanModelTypeOptions = mapOf (
     SmartScanModelType.TEXT_ENCODER to "Text encoder"
 )
 
+// Models Info
 const val MODEL_DIR = "models"
+const val INCEPTION_RESNET_DEP_FACE_DETECTOR = "face_detect.onnx"
+const val INCEPTION_RESNET_DEP_INCEPTION = "inception_resnet_v1_quant.onnx"
 
-val modelPathsMap = mapOf(
-    SmartScanModelType.FACE to ModelPathInfo(
-        path = "$MODEL_DIR/${SmartScanModelType.FACE.tag}.zip",
-        dependentModelPaths = listOf(
-            "$MODEL_DIR/${SmartScanModelType.FACE.tag}/face_detect.onnx",
-            "$MODEL_DIR/${SmartScanModelType.FACE.tag}/inception_resnet_v1_quant.onnx"
-        )
-    ),
-    SmartScanModelType.OBJECTS to ModelPathInfo("$MODEL_DIR/${SmartScanModelType.OBJECTS.tag}.onnx"),
-    SmartScanModelType.IMAGE_ENCODER to ModelPathInfo("$MODEL_DIR/${SmartScanModelType.IMAGE_ENCODER.tag}.onnx"),
-    SmartScanModelType.TEXT_ENCODER to ModelPathInfo("$MODEL_DIR/${SmartScanModelType.TEXT_ENCODER.tag}.onnx")
+val facialRecognitionModel = ModelInfo(
+    type = SmartScanModelType.FACE,
+    name = "Inception Resnet V1",
+    url = "https://github.com/dev-diaries41/smartscan-models/releases/download/1.0.0/facial_recognition_inception_resnet_v1.zip",
 )
+
+val miniLmTextEmbedderModel = ModelInfo(
+    type = SmartScanModelType.TEXT_ENCODER,
+    name = "MiniLM-L6-v2",
+    url = "https://github.com/dev-diaries41/smartscan-models/releases/download/1.0.0/minilm_sentence_transformer_quant.onnx",
+)
+
+val downloadableModels = listOf(facialRecognitionModel, miniLmTextEmbedderModel)
+
